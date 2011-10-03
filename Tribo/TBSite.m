abfe6f73ec603e7dc67d5e6610fb7b59050b38e0
George Woodliff-Stanley <gmws82@me.com>
2011-10-02 20:04:19 -0600

    Added a "lastPost" property to TBSite.

--- Tribo/TBSite.m
+++ Tribo/TBSite.m
@@ -11,6 +11,7 @@
 #import "GRMustache.h"
 
 @interface TBSite ()
+@property (nonatomic, strong) TBPost *lastPost;
 @property (nonatomic, strong) NSArray *recentPosts;
 @property (nonatomic, strong) GRMustacheTemplate *postTemplate;
 - (void)parsePosts;
@@ -24,6 +25,7 @@
 @synthesize postsDirectory=_postsDirectory;
 @synthesize templatesDirectory=_templatesDirectory;
 @synthesize posts=_posts;
+@synthesize lastPost=_lastPost;
 @synthesize recentPosts=_recentPosts;
 @synthesize postTemplate=_postTemplate;
 - (void)process {
@@ -85,6 +87,7 @@
 	}
 	self.posts = [NSMutableArray arrayWithArray:[[self.posts reverseObjectEnumerator] allObjects]];
 	self.recentPosts = [self.posts subarrayWithRange:NSMakeRange(0, 5)];
+    self.lastPost = [self.recentPosts objectAtIndex:0];
 }
 - (void)writePosts {
 	
