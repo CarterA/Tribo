07e0a161ebe489f529135489c1474f3add3853ad
Carter Allen <CarterA@opt-6.com>
2012-03-01 09:37:38 -0700

    Created the UI for configuring publishing.
    The UI is currently completely detached from the actual publisher.
    Putting it all together comes next.

--- Mac App/Controllers/TBSettingsSheetController.m
+++ Mac App/Controllers/TBSettingsSheetController.m
@@ -9,24 +9,49 @@
 #import "TBSettingsSheetController.h"
 #import "TBSite.h"
 
-@interface TBSettingsSheetController ()
+@interface TBSettingsSheetController () <NSTextFieldDelegate>
+
 @property (nonatomic, assign) IBOutlet NSTextField *siteNameField;
 @property (nonatomic, assign) IBOutlet NSTextField *authorField;
 @property (nonatomic, assign) IBOutlet NSTextField *baseURLField;
 @property (nonatomic, assign) IBOutlet NSTextField *numberOfRecentPostsField;
 @property (nonatomic, assign) IBOutlet NSStepper *recentPostsStepper;
+
+@property (nonatomic, assign) IBOutlet NSPopUpButton *uploadViaPopUp;
+@property (nonatomic, assign) IBOutlet NSTextField *serverField;
+@property (nonatomic, assign) IBOutlet NSTextField *portField;
+@property (nonatomic, assign) IBOutlet NSTextField *userNameField;
+@property (nonatomic, assign) IBOutlet NSSecureTextField *passwordField;
+@property (nonatomic, assign) IBOutlet NSButton *rememberInKeychainCheckbox;
+@property (nonatomic, assign) IBOutlet NSTextField *remotePathField;
+
 - (void)didEndSheet:(NSWindow *)sheet returnCode:(NSInteger)returnCode contextInfo:(void *)contextInfo;
 - (IBAction)save:(id)sender;
 - (IBAction)cancel:(id)sender;
+- (IBAction)uploadViaPopUpDidChange:(id)sender;
+
+- (void)loadFormValues;
+- (void)updatePlaceholders;
+
 @end
 
 @implementation TBSettingsSheetController
+
 @synthesize site = _site;
+
 @synthesize siteNameField = _siteNameField;
 @synthesize authorField = _authorField;
 @synthesize baseURLField = _baseURLField;
 @synthesize numberOfRecentPostsField = _numberOfRecentPostsField;
 @synthesize recentPostsStepper = _recentPostsStepper;
+
+@synthesize uploadViaPopUp = _uploadViaPopUp;
+@synthesize serverField = _serverField;
+@synthesize portField = _portField;
+@synthesize userNameField = _userNameField;
+@synthesize passwordField = _passwordField;
+@synthesize rememberInKeychainCheckbox = _rememberInKeychainCheckbox;
+@synthesize remotePathField = _remotePathField;
 
 - (id)init {
     self = [super initWithWindowNibName:@"TBSettingsSheet"];
@@ -36,14 +61,46 @@
 - (void)runModalForWindow:(NSWindow *)window site:(TBSite *)site {
 	[self loadWindow];
 	self.site = site;
-	NSDictionary *metadata = site.metadata;
+	[self loadFormValues];
+	[self updatePlaceholders];
+	[NSApp beginSheet:self.window modalForWindow:window modalDelegate:self didEndSelector:@selector(didEndSheet:returnCode:contextInfo:) contextInfo:nil];
+}
+
+- (IBAction)uploadViaPopUpDidChange:(id)sender {
+	[self updatePlaceholders];
+}
+
+- (void)loadFormValues {
+	NSDictionary *metadata = self.site.metadata;
 	self.siteNameField.stringValue = [metadata objectForKey:TBSiteNameMetadataKey] ?: @"";
 	self.authorField.stringValue = [metadata objectForKey:TBSiteAuthorMetadataKey] ?: @"";
 	self.baseURLField.stringValue = [metadata objectForKey:TBSiteBaseURLMetadataKey] ?: @"";
 	NSNumber *numberOfRecentPosts = [metadata objectForKey:TBSiteNumberOfRecentPostsMetadataKey] ?: [NSNumber numberWithInteger:5];
 	self.numberOfRecentPostsField.objectValue = numberOfRecentPosts;
 	self.recentPostsStepper.objectValue = numberOfRecentPosts;
-	[NSApp beginSheet:self.window modalForWindow:window modalDelegate:self didEndSelector:@selector(didEndSheet:returnCode:contextInfo:) contextInfo:nil];
+	
+}
+
+- (void)updatePlaceholders {
+	[self.userNameField.cell setPlaceholderString:NSUserName()];
+	NSInteger port = 0;
+	if ([self.uploadViaPopUp.titleOfSelectedItem isEqualToString:@"FTP"]) {
+		port = 21;
+		[self.passwordField.cell setPlaceholderString:@""];
+	}
+	else if ([self.uploadViaPopUp.titleOfSelectedItem isEqualToString:@"SFTP"]) {
+		port = 22;
+		[self.passwordField.cell setPlaceholderString:@"Key-based authentication"];
+	}
+	[self.portField.cell setPlaceholderString:[NSString stringWithFormat:@"%d", port]];
+	if (self.passwordField.stringValue.length > 0)
+		self.rememberInKeychainCheckbox.enabled = YES;
+	else
+		self.rememberInKeychainCheckbox.enabled = NO;
+}
+
+- (void)controlTextDidChange:(NSNotification *)notification {
+	[self updatePlaceholders];
 }
 
 - (IBAction)save:(id)sender {
