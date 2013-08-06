f367b172fa26d92524755f67de9eb12702a30324
Carter Allen <CarterA@opt-6.com>
2013-08-05 22:07:35 -0600

    Added a settings UI for filters.

--- Mac App/Controllers/TBSettingsSheetController.m
+++ Mac App/Controllers/TBSettingsSheetController.m
@@ -11,13 +11,15 @@
 #import "TBSite.h"
 #import <Security/Security.h>
 
-@interface TBSettingsSheetController () <NSTextFieldDelegate>
+@interface TBSettingsSheetController () <NSTextFieldDelegate, NSTableViewDelegate, NSTableViewDataSource>
 
 @property (nonatomic, assign) IBOutlet NSTextField *siteNameField;
 @property (nonatomic, assign) IBOutlet NSTextField *authorField;
 @property (nonatomic, assign) IBOutlet NSTextField *baseURLField;
 @property (nonatomic, assign) IBOutlet NSTextField *numberOfRecentPostsField;
 @property (nonatomic, assign) IBOutlet NSStepper *recentPostsStepper;
+
+@property (nonatomic, assign) IBOutlet NSTableView *filtersTable;
 
 @property (nonatomic, assign) IBOutlet NSPopUpButton *uploadViaPopUp;
 @property (nonatomic, assign) IBOutlet NSTextField *serverField;
@@ -26,7 +28,11 @@
 @property (nonatomic, assign) IBOutlet NSSecureTextField *passwordField;
 @property (nonatomic, assign) IBOutlet NSTextField *remotePathField;
 
+@property (nonatomic, strong) NSArray *availableFilters;
+@property (nonatomic, strong) NSMutableArray *enabledFilters;
+
 - (void)didEndSheet:(NSWindow *)sheet returnCode:(NSInteger)returnCode contextInfo:(void *)contextInfo;
+- (IBAction)revealFiltersFolder:(id)sender;
 - (IBAction)save:(id)sender;
 - (IBAction)cancel:(id)sender;
 - (IBAction)uploadViaPopUpDidChange:(id)sender;
@@ -48,11 +54,18 @@
 }
 
 - (void)runModalForWindow:(NSWindow *)window site:(TBSite *)site {
-	[self loadWindow];
 	self.site = site;
+	[self loadFilters];
+	[self loadWindow];
 	[self loadFormValues];
 	[self updatePlaceholders];
+	[self.filtersTable reloadData];
 	[NSApp beginSheet:self.window modalForWindow:window modalDelegate:self didEndSelector:@selector(didEndSheet:returnCode:contextInfo:) contextInfo:nil];
+}
+
+- (IBAction)revealFiltersFolder:(id)sender {
+	NSURL *scriptsDirectory = [[NSFileManager defaultManager] URLForDirectory:NSApplicationScriptsDirectory inDomain:NSUserDomainMask appropriateForURL:nil create:YES error:nil];
+	[[NSWorkspace sharedWorkspace] selectFile:scriptsDirectory.path inFileViewerRootedAtPath:nil];
 }
 
 - (IBAction)uploadViaPopUpDidChange:(id)sender {
@@ -87,6 +100,17 @@
 	
 }
 
+- (void)loadFilters {
+	NSURL *scriptsDirectory = [[NSFileManager defaultManager] URLForDirectory:NSApplicationScriptsDirectory inDomain:NSUserDomainMask appropriateForURL:nil create:YES error:nil];
+	NSArray *filterURLs = [[NSFileManager defaultManager] contentsOfDirectoryAtURL:scriptsDirectory includingPropertiesForKeys:@[NSURLNameKey] options:NSDirectoryEnumerationSkipsHiddenFiles error:nil];
+	NSMutableArray *filterNames = [NSMutableArray array];
+	for (NSURL *filterURL in filterURLs) {
+		[filterNames addObject:[filterURL resourceValuesForKeys:@[NSURLNameKey] error:nil][NSURLNameKey]];
+	}
+	self.availableFilters = filterNames;
+	self.enabledFilters = [self.site.metadata[TBSiteFilters] mutableCopy] ?: [NSMutableArray array];
+}
+
 - (void)updatePlaceholders {
 	[self.userNameField.cell setPlaceholderString:NSUserName()];
 	NSInteger port = 0;
@@ -117,6 +141,7 @@
 							TBSiteAuthorMetadataKey: self.authorField.stringValue,
 							TBSiteBaseURLMetadataKey: self.baseURLField.stringValue,
 							TBSiteNumberOfRecentPostsMetadataKey: numberOfRecentPosts,
+							TBSiteFilters: self.enabledFilters,
 							TBSiteProtocolKey: protocol,
 							TBSiteServerKey: self.serverField.stringValue,
 							TBSitePortKey: self.portField.stringValue,
@@ -202,6 +227,30 @@
 	if (returnCode != NSOKButton) return;
 	self.site.metadata = [self dictionaryOfFormValues];
 	[self setStoredPassword:self.passwordField.stringValue];
+}
+
+# pragma mark - NSTableViewDelegate
+
+- (NSInteger)numberOfRowsInTableView:(NSTableView *)tableView {
+	return [self.availableFilters count];
+}
+
+- (id)tableView:(NSTableView *)tableView objectValueForTableColumn:(NSTableColumn *)tableColumn row:(NSInteger)row {
+	NSString *filterName = self.availableFilters[row];
+	if ([tableColumn.identifier isEqualToString:@"checkbox"]) {
+		return @([self.enabledFilters containsObject:filterName]);
+	}
+	else if ([tableColumn.identifier isEqualToString:@"name"])
+		return filterName;
+	return nil;
+}
+
+- (void)tableView:(NSTableView *)tableView setObjectValue:(id)object forTableColumn:(NSTableColumn *)tableColumn row:(NSInteger)row {
+	NSString *filterName = self.availableFilters[row];
+	if ([object boolValue])
+		[self.enabledFilters addObject:filterName];
+	else
+		[self.enabledFilters removeObject:filterName];
 }
 
 @end
