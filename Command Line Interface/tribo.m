9b779086b9820b0618da009fd5f0e55412a2b456
Carter Allen <CarterA@opt-6.com>
2013-08-08 16:21:16 -0600

    Added live reload support to the CLI.
    This was pretty tricky. The two LiveReload files had to be placed inside the Info.plist file, which is then embedded into the __TEXT section of the CLI. I can then access the keys inside via NSBundle methods. It’s a bit of a hack, but it keeps the CLI self-contained and doesn’t duplicate any data.

--- Command Line Interface/tribo.m
+++ Command Line Interface/tribo.m
@@ -8,47 +8,55 @@
 //
 
 #import "TBSite.h"
-#import "HTTPServer.h"
+#import "TBHTTPServer.h"
+#import "TBSocketConnection.h"
+#import "CZAFileWatcher.h"
 #import <ApplicationServices/ApplicationServices.h>
 
 void printHeader(const char *header);
 void printError(const char *errorMessage);
 
 int main (int argumentCount, const char *arguments[]) {
+	
 	@autoreleasepool {
-		NSRunLoop *runLoop = [NSRunLoop mainRunLoop];
+		
 		printHeader("Compiling website...");
-		TBSite *site = [TBSite new];
-		site.root = [NSURL fileURLWithPath:[NSFileManager defaultManager].currentDirectoryPath];
-		site.destination = [site.root URLByAppendingPathComponent:@"Output" isDirectory:YES];
-		site.sourceDirectory = [site.root URLByAppendingPathComponent:@"Source" isDirectory:YES];
-		site.postsDirectory = [site.root URLByAppendingPathComponent:@"Posts" isDirectory:YES];
-		site.templatesDirectory = [site.root URLByAppendingPathComponent:@"Templates" isDirectory:YES];
+		TBSite *site = [TBSite siteWithRoot:[NSURL fileURLWithPath:[NSFileManager defaultManager].currentDirectoryPath]];
         
         NSError *error = nil;
-		BOOL websiteBuilt = [site process:&error];
-        if (!websiteBuilt) {
+        if (![site process:&error]) {
             printError([[error localizedDescription] cStringUsingEncoding:NSUTF8StringEncoding]);
             exit((int)[error code]);
         }
         
-		HTTPServer *server = [HTTPServer new];
+		TBHTTPServer *server = [TBHTTPServer new];
+		server.connectionClass = [TBSocketConnection class];
 		server.documentRoot = site.destination.path;
 		server.port = 4000;
 		[server start:nil];
 		printHeader("Development server started at http://localhost:4000/");
 		printHeader("Opening website in default web browser...");
 		LSOpenCFURLRef((__bridge CFURLRef)[NSURL URLWithString:@"http://localhost:4000/"], NULL);
-		[runLoop run];
+		
+		CZAFileWatcher *watcher = [CZAFileWatcher fileWatcherForURLs:@[site.sourceDirectory, site.postsDirectory, site.templatesDirectory] changesHandler:^(NSArray *changedURLs) {
+			[site process:nil];
+			[server refreshPages];
+		}];
+		[watcher startWatching];
+		
+		dispatch_main();
+		
 	}
+	
     return EXIT_SUCCESS;
+	
 }
 
 void printHeader(const char *header) {
 	printf("\e[1m\e[34m==>\e[0m\e[0m ");
 	printf("\e[1m%s\e[0m\n", header);
 }
-// Maybe you wanna put in special error color codes?
+
 void printError(const char *errorMessage) {
     fprintf(stderr, "\e[1m\e[34m==>\e[0m\e[0m ");
 	fprintf(stderr, "\e[1m%s\e[0m\n", errorMessage);
